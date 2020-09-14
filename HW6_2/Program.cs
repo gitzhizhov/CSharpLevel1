fbcb958d62c0224a1bd97ae0b93aec660b0cd4fe
Andrey Zhizhov <gitzhizhov@github.com>
2020-09-15 00:36:10 +0500

    ДЗ 6 выполнил

---
+++ HW6_2/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6_2
+{
+    //2.    Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+    //      а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум.
+    //      Использовать массив(или список) делегатов, в котором хранятся различные функции.
+    //      б) * Переделать функцию Load, чтобы она возвращала массив считанных значений.
+    //      Пусть она возвращает минимум через параметр(с использованием модификатора out). 
+
+    // Андрей Жижов
+
+    class Program
+    {
+        /// <summary>
+        /// Делегат функции с сигнатурой double
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public delegate double Fun(double x);
+
+        /// <summary>
+        /// Расчет значений переданной функции, запись в файл в бинарном потоке
+        /// </summary>
+        /// <param name="fileName">Имя файла</param>
+        /// <param name="start">Начало отрезка</param>
+        /// <param name="end">Конец отрезка</param>
+        /// <param name="step">Шаг</param>
+        public static void SaveFunc(string fileName, double start, double end, double step, Fun F)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = start;
+            while (x <= end)
+            {
+                bw.Write(F(x));
+                x += step;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        /// <summary>
+        /// Чтение файла, нахождение минимума
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static double[] Load(string fileName, out double min)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            min = double.MaxValue;
+            double[] array = new double[fs.Length / sizeof(double)];
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                array[i] = d;
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return array;
+        }
+
+        /// <summary>
+        /// Метод возвращает выбранную функцию списка
+        /// </summary>
+        /// <param name="listFunc"></param>
+        /// <returns></returns>
+        private static Fun GetFunc(List<Fun> listFunc)
+        {
+            int max = listFunc.Count;
+            while (true)
+            {
+                //int x = Convert.ToInt32(Console.ReadLine());
+                if (!int.TryParse(Console.ReadLine(), out int x) || x > max)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Введите целое число 1 до {max}.");
+                    Console.WriteLine("Повторите ввод.");
+                    Console.WriteLine();
+                    Welcome();
+                }
+                else
+                    return listFunc[x - 1];
+            }
+        }
+
+        private static void SetInterval(out double start, out double end)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Задайте отрезок для нахождения минимума функции: ");
+            Console.Write("Введите начало отрезка: ");
+            start = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите конец отрезка: ");
+            end = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Вывод результата
+        /// </summary>
+        /// <param name="values"></param>
+        private static void Print(double start, double end, double step, double[] values)
+        {
+            Console.WriteLine("------- X ------- Y -----");
+            int index = 0;
+            while (start <= end)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} ", start, values[index]);
+                start += step;
+                index++;
+            }
+            Console.WriteLine("--------------------------");
+        }
+
+        /// <summary>
+        /// Приветствие
+        /// </summary>
+        private static void Welcome()
+        {
+            Console.WriteLine("Выберете функция для работы.");
+            Console.WriteLine("1: x^2 - 50*x + 10");
+            Console.WriteLine("2: (x^2) / 4 - x^3");
+            Console.WriteLine("3: (x-1)^3");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            double start = 0;
+            double end = 0;
+            double step = 0.5;
+            string path = "data.bin";
+            double min = double.MaxValue;
+
+            List<Fun> listFunc = new List<Fun>();
+            listFunc.Add(Functoins.F1);
+            listFunc.Add(Functoins.F2);
+            listFunc.Add(Functoins.F3);
+
+            Welcome();
+            Fun useFunc = GetFunc(listFunc);
+            SetInterval(out start, out end);
+
+            SaveFunc(path, start, end, step, useFunc);
+            Print(start, end, step, Load(path, out min));
+            Console.WriteLine($"Минимальное значение = {min}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW6_3/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6_3
+{
+    //    3.    Переделать программу Пример использования коллекций для решения следующих задач:
+    //          а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+    //          б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся(*частотный массив);
+    //          в) отсортировать список по возрасту студента;
+    //          г) * отсортировать список по курсу и возрасту студента;
+
+    class Program
+    {
+        /// <summary>
+        /// Метод для сравнения экземпляров, по имени
+        /// </summary>
+        /// <param name="st1"></param>
+        /// <param name="st2"></param>
+        /// <returns></returns>
+        static int MyDelegat(Student st1, Student st2)          // Создаем метод для сравнения для экземпляров
+        {
+            return String.Compare(st1.firstName, st2.firstName);          // Сравниваем две строки
+        }
+
+        /// <summary>
+        /// Метод для сравнения экземпляров, по курсу
+        /// </summary>
+        /// <param name="st1"></param>
+        /// <param name="st2"></param>
+        /// <returns></returns>
+        static int DelegateCours(Student st1, Student st2)
+        {
+            return String.Compare(st1.course.ToString(), st2.course.ToString());
+        }
+
+        /// <summary>
+        /// Метод для сравнения экземпляров, по возрасту
+        /// </summary>
+        /// <param name="st1"></param>
+        /// <param name="st2"></param>
+        /// <returns></returns>
+        static int DelegateAge(Student st1, Student st2)
+        {
+            return String.Compare(st1.age.ToString(), st2.age.ToString());
+        }
+
+        /// <summary>
+        /// Метод для сравнения экземпляров по курсу и возрасту
+        /// Пример сортировки по 2-м параметрам
+        /// </summary>
+        /// <param name="st1"></param>
+        /// <param name="st2"></param>
+        /// <returns></returns>
+        static int DelegateCourseAndAge(Student st1, Student st2)
+        {
+            if (st1.course > st2.course)
+                return 1;
+            if (st1.course < st2.course)
+                return -1;
+            if (st1.age > st2.age)
+                return 1;
+            if (st1.age < st2.age)
+                return -1;
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int student5Cours = 0;
+            int student6Cours = 0;
+            int bakalavr = 0;
+            int magistr = 0;
+            Dictionary<int, int> counsStudents = new Dictionary<int, int>();
+            List<Student> list = new List<Student>();                             // Создаем список студентов
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("students_6.csv");
+
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                    // Добавляем в список новый экземпляр класса Student
+                    list.Add(new Student(s[0], s[1], s[2], s[3], int.Parse(s[4]), s[5], int.Parse(s[6]), s[7], int.Parse(s[8])));
+                    // Одновременно подсчитываем количество бакалавров и магистров
+                    if (int.Parse(s[4]) < 5) bakalavr++; else magistr++;
+                    // Одновременно подсчитываем количество студентов учащихся на 5 и 6 курсах
+                    if (int.Parse(s[4]) == 5) student5Cours++;
+                    if (int.Parse(s[4]) == 6) student6Cours++;
+                    // частотный анализ студентов на курсах
+                    if (int.Parse(s[8]) >= 18 && int.Parse(s[8]) <= 20)
+                        if (counsStudents.ContainsKey(int.Parse(s[4])))
+                            counsStudents[int.Parse(s[4])]++;
+                        else
+                            counsStudents.Add(int.Parse(s[4]), 1);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
+                    // Выход из Main
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
+            }
+            sr.Close();
+
+
+            Console.WriteLine("Всего студентов:" + list.Count);
+            Console.WriteLine("Магистров:{0}", magistr);
+            Console.WriteLine("Бакалавров:{0}", bakalavr);
+            foreach (var v in list) Console.WriteLine(v.firstName);
+
+            Console.WriteLine();
+            Console.WriteLine($"Количество студентов учащихся на 5 курсе: {student5Cours};");
+            Console.WriteLine($"Количество студентов учащихся на 6 курсе: {student6Cours};");
+            Console.WriteLine();
+
+            Console.WriteLine("Количество студентов в возрасте от 18 до 20 лет на каком курсе учатся");
+            foreach (KeyValuePair<int, int> keyValue in counsStudents)
+            {
+                Console.WriteLine($"Курс {keyValue.Key} - кол. {keyValue.Value}");
+            }
+
+            Console.WriteLine("Сортировка по возрасту");
+            list.Sort(new Comparison<Student>(DelegateAge));
+            foreach (var v in list) Console.WriteLine(v.firstName);
+            Console.WriteLine();
+
+            Console.WriteLine("Сортировка по курсу и возрасту");
+            list.Sort(new Comparison<Student>(DelegateCourseAndAge));
+            foreach (var v in list) Console.WriteLine(v.firstName);
+
+            Console.WriteLine();
+            Console.WriteLine(DateTime.Now - dt);
+            Console.ReadKey();
+        }
+    }
+
+}
